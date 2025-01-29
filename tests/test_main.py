74c75300836df5efa8de140563f0507c25dd3da0
anastasia <wwwiii1982@mail.ru>
2025-01-29 19:52:54 +0300

    test_1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,7 @@
 import pytest
-from src.main import (
-    BadClass,
-    compute_other_thing,
-    viking_cafe_order,
-)
+
+from src.main import BadClass, compute_other_thing, viking_cafe_order
+
 
 def test_bad_class_get_value() -> None:  # Добавлена аннотация типа -> None
     bad_class_instance = BadClass()
@@ -12,17 +10,17 @@ def test_bad_class_get_value() -> None:  # Добавлена аннотация
         result == "some_other_value"
     ), f"Expected 'some_other_value', but got {result}"
 
+
 def test_bad_class_compute_something() -> None:  # Добавлена аннотация типа -> None
     bad_class_instance = BadClass()
     result = bad_class_instance.compute_something()
     assert result is True, f"Expected True, but got {result}"
 
+
 def test_bad_class_it_will_fail() -> None:  # Добавлена аннотация типа -> None
     bad_class_instance = BadClass()
     with pytest.raises(AttributeError):
         bad_class_instance.it_will_fail()
-
-
 
 
 def test_viking_cafe_order():
