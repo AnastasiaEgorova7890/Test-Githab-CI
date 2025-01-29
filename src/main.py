beba19b7140e01b39b075a246d306076bb264174
anastasia <wwwiii1982@mail.ru>
2025-01-29 19:40:26 +0300

    test_1

--- src/main.py
+++ src/main.py
@@ -6,7 +6,23 @@ import flask
 import tempfile
 import sys
 
-from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14
+from third_party import (
+    lib15,
+    lib1,
+    lib2,
+    lib3,
+    lib4,
+    lib5,
+    lib6,
+    lib7,
+    lib8,
+    lib9,
+    lib10,
+    lib11,
+    lib12,
+    lib13,
+    lib14,
+)
 
 
 class BadClass:
